4206e637bc9f46ee069d5140ba43356907a21552
Thy Tran <thytrannk@gmail.com>
2018-11-15 10:32:12 -0800

    add photos and importance map

--- project.cpp
+++ project.cpp
@@ -288,7 +288,7 @@ int main(int argc, char **argv) {
 //    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     // read in image
     FreeImage_Initialise();
-    strcpy(filename, "house.jpg");
+    strcpy(filename, "house-bw-noal.png");
     loadImage(filename, imageSizeX, imageSizeY, image_bpp, image_nChannel, &image, &img);
     cleanOutput();
     // generate QR code encoding a text
