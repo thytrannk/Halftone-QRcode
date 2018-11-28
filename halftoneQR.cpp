bceca48f204213479ed83e94cb628658c135f3d2
Thy Tran <thytrannk@gmail.com>
2018-11-27 22:39:25 -0700

    changed to 4 channels, not distorted anymore but code is unstable. sometimes give all black for rescaled image, sometimes rescaling works but qr fails, sometimes malloc free fails

--- halftoneQR.cpp
+++ halftoneQR.cpp
@@ -7,61 +7,61 @@
 #include "project.h"
 #include "halftone.h"
 
-enum position {
-    topleft,
-    topmid,
-    topright,
-    centerleft,
-    centermid,
-    centerright,
-    botleft,
-    botmid,
-    botright,
-};
+//enum position {
+//    topleft,
+//    topmid,
+//    topright,
+//    centerleft,
+//    centermid,
+//    centerright,
+//    botleft,
+//    botmid,
+//    botright,
+//};
 
 int startX_img, startY_img, startX_qr, startY_qr; // start location of mapping for image and qr code
 int x, y; // width and height of mapping area
 
-static void originalCoor2Coor (int originalX, int originalY, enum position position1, int &x, int &y) {
-    switch (position1) {
-        case topleft:
-            x = originalX * 3;
-            y = originalY * 3;
-            break;
-        case topmid:
-            x = originalX * 3 + 1;
-            y = originalY * 3;
-            break;
-        case topright:
-            x = originalX * 3 + 2;
-            y = originalY * 3;
-            break;
-        case centerleft:
-            x = originalX * 3;
-            y = originalY * 3 + 1;
-            break;
-        case centermid:
-            x = originalX * 3 + 1;
-            y = originalY * 3 + 1;
-            break;
-        case centerright:
-            x = originalX * 3 + 2;
-            y = originalY * 3 + 1;
-            break;
-        case botleft:
-            x = originalX * 3;
-            y = originalY * 3 + 2;
-            break;
-        case botmid:
-            x = originalX * 3 + 1;
-            y = originalY * 3 + 2;
-            break;
-        case botright:
-            x = originalX * 3 + 2;
-            y = originalY * 3 + 2;
-            break;
-    }
-}
+//static void originalCoor2Coor (int originalX, int originalY, enum position position1, int &x, int &y) {
+//    switch (position1) {
+//        case topleft:
+//            x = originalX * 3;
+//            y = originalY * 3;
+//            break;
+//        case topmid:
+//            x = originalX * 3 + 1;
+//            y = originalY * 3;
+//            break;
+//        case topright:
+//            x = originalX * 3 + 2;
+//            y = originalY * 3;
+//            break;
+//        case centerleft:
+//            x = originalX * 3;
+//            y = originalY * 3 + 1;
+//            break;
+//        case centermid:
+//            x = originalX * 3 + 1;
+//            y = originalY * 3 + 1;
+//            break;
+//        case centerright:
+//            x = originalX * 3 + 2;
+//            y = originalY * 3 + 1;
+//            break;
+//        case botleft:
+//            x = originalX * 3;
+//            y = originalY * 3 + 2;
+//            break;
+//        case botmid:
+//            x = originalX * 3 + 1;
+//            y = originalY * 3 + 2;
+//            break;
+//        case botright:
+//            x = originalX * 3 + 2;
+//            y = originalY * 3 + 2;
+//            break;
+//    }
+//}
 
 void QRtimes3(void) {
     int originalWidth = qrCode->width;
@@ -71,41 +71,35 @@ void QRtimes3(void) {
     halftonedQRCode->width = newWidth;
     halftonedQRCode->data = new unsigned char[newWidth*newWidth];
 
-    for (int y = 0; y < originalWidth; y++) {
-        for (int x = 0; x < originalWidth; x++) {
-            int newX, newY;
-            originalCoor2Coor(x, y, topleft, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, topmid, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, topright, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, centerleft, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, centermid, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, centerright, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, botleft, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, botmid, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-            originalCoor2Coor(x, y, botright, newX, newY);
-            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
-
-//            if ((qrCode->data[y * originalWidth + x] & (11 << 1))) {
-////                newQR->data[y * newWidth + x] = qrCode->data[y * newWidth + x] | 1; // turn to black
-////                newQR->data[y * newWidth + x] = qrCode->data[y * newWidth + x] & 11111110; // turn to white
-//                halftonedQRCode->data[y * newWidth + x] = qrCode->data[y * newWidth + x];
-//            }
-        }
-    }
-
-//    for (int y = 0; y < newWidth; y++) {
-//        for (int x = 0; x < newWidth; x++) {
-//            halftonedQRCode->data[y * newWidth + x] = qrCode->data[((int) (y/3)) * originalWidth + ((int) (x/3))];
+//    for (int y = 0; y < originalWidth; y++) {
+//        for (int x = 0; x < originalWidth; x++) {
+//            int newX, newY;
+//            originalCoor2Coor(x, y, topleft, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, topmid, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, topright, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, centerleft, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, centermid, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, centerright, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, botleft, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, botmid, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
+//            originalCoor2Coor(x, y, botright, newX, newY);
+//            halftonedQRCode->data[newY * newWidth + newX] = qrCode->data[y * originalWidth + x];
 //        }
 //    }
+
+    for (int y = 0; y < newWidth; y++) {
+        for (int x = 0; x < newWidth; x++) {
+            halftonedQRCode->data[y * newWidth + x] = qrCode->data[((int) (y/3)) * originalWidth + ((int) (x/3))];
+        }
+    }
 }
 
 void calcStart(void) {
@@ -155,11 +149,15 @@ void *halftoneQR(void) {
             }
             if (!halftone[(y_img * imageSizeX + x_img) * image_nChannel]) {
                 // halftone bit is black
+//                halftonedQRCode->data[y_qr * width_qr + x_qr] =
+//                        (unsigned char) (((halftonedQRCode->data[y_qr * width_qr + x_qr] >> 1) << 1) + 1);
                 halftonedQRCode->data[y_qr * width_qr + x_qr] =
-                        (unsigned char) (((halftonedQRCode->data[y_qr * width_qr + x_qr] >> 1) << 1) + 1);
+                        (unsigned char) (halftonedQRCode->data[y_qr * width_qr + x_qr] | 1);
             } else {
+//                halftonedQRCode->data[y_qr * width_qr + x_qr] =
+//                        (halftonedQRCode->data[y_qr * width_qr + x_qr] >> 1) << 1;
                 halftonedQRCode->data[y_qr * width_qr + x_qr] =
-                        (halftonedQRCode->data[y_qr * width_qr + x_qr] >> 1) << 1;
+                        (unsigned char) (halftonedQRCode->data[y_qr * width_qr + x_qr] & 11111110);
             }
         }
     }
--- project.cpp
+++ project.cpp
@@ -11,77 +11,83 @@
 using namespace std;
 
 // define extern variables
-int imageSizeX, imageSizeY, impSizeX, impSizeY, qrSizeX, qrSizeY;
-BYTE *image, *imp_map, *qr_image; // pointer to the image
+int imageSizeX, imageSizeY;
+BYTE *image; // pointer to the image
 BYTE *halftone; // pointer to the halftone image
 int image_bpp, image_nChannel, imp_bpp, imp_nChannel, qr_bpp, qr_nChannel;
-FIBITMAP *img, *imp, *qr_img;
+FIBITMAP *img;
 char filename[100], qrFilename[100], qrText[100];
 QRcode *qrCode, *halftonedQRCode;
 int threshold = 128; // initial threshold for floyd steinberg
 
-// swap the red and blue channels - this is because
-// FreeImage loads the image in the format BGR and not RGB as used in OpenGL
-void swapRedwithBlue(BYTE *imageptr, int sizeX, int sizeY){
-	// need to swap the channels	
-	BYTE a;
-	for (int i = 0; i < sizeY; i++){
-		for (int j = 0; j < sizeX; j++){
-			a = imageptr[(i*sizeX + j) * 3]; // blue
-			imageptr[(i*sizeX + j) * 3] = imageptr[(i*sizeX + j)*3+ 2];
-			imageptr[(i*sizeX + j) * 3 + 2] = a;
-		}
-	}
-}
-// load an image (can be an image to be halftoned, or a QR image)
+//// swap the red and blue channels - this is because
+//// FreeImage loads the image in the format BGR and not RGB as used in OpenGL
+//void swapRedwithBlue(BYTE *imageptr, int sizeX, int sizeY){
+//	// need to swap the channels
+//	BYTE a;
+//	for (int i = 0; i < sizeY; i++){
+//		for (int j = 0; j < sizeX; j++){
+//			a = imageptr[(i*sizeX + j) * 3]; // blue
+//			imageptr[(i*sizeX + j) * 3] = imageptr[(i*sizeX + j)*3+ 2];
+//			imageptr[(i*sizeX + j) * 3 + 2] = a;
+//		}
+//	}
+//}
+// load an image
 void loadImage (char *filename, int &sizeX, int &sizeY, int &bpp, int &nChannel, BYTE **imageptr, FIBITMAP **img) {
     FREE_IMAGE_FORMAT formato = FreeImage_GetFIFFromFilename(filename);
     if (formato == FIF_UNKNOWN) {
-        cout << "image format unknown\n";
+        cout << "Image format is unknown!\n";
         exit(1);
     }
     if (!(*img)) FreeImage_Unload(*img);
-    *img = FreeImage_Load(formato, filename);
+    *img = FreeImage_Load(formato, filename, RAW_DISPLAY);
     if (!(*img)) {
-        cout << "image not found!";
+        cout << "Image cannot be found!";
         exit(1);
     }
+    // Convert non-32 bit images
+    if (FreeImage_GetBPP(*img) != 32) {
+        FIBITMAP* oldImage = *img;
+        *img = FreeImage_ConvertTo32Bits(oldImage);
+        FreeImage_Unload(oldImage);
+    }
+
     sizeX = FreeImage_GetWidth(*img);
     sizeY = FreeImage_GetHeight(*img);
     bpp = FreeImage_GetBPP(*img);
     nChannel = bpp / 8;
     cout << "X =" << sizeX << " Y = " << sizeY << " bpp =" << bpp << " nChannel =" << nChannel << "\n";
-    if (nChannel == 1) {
-        cout << "convert to 24 bit before running this program\n";
-        exit(1);
-    } else {
-        cout << "Do you want to rescale the image? (y/n) ";
-        char rescale;
-        cin >> rescale;
-        if (rescale == 'y') {
-            cout << "New width: ";
-            int width;
-            cin >> width;
-            cout << "New height: ";
-            int height;
-            cin >> height;
-            *img = FreeImage_Rescale(*img, width, height, FILTER_BILINEAR);
-            sizeX = FreeImage_GetWidth(*img);
-            sizeY = FreeImage_GetHeight(*img);
-            bpp = FreeImage_GetBPP(*img);
-            nChannel = bpp / 8;
-            cout << "X =" << sizeX << " Y = " << sizeY << " bpp =" << bpp << " nChannel =" << nChannel << "\n";
-        }
-        if (!(*imageptr)) {
-            delete[] (*imageptr);
-        }
-        *imageptr = FreeImage_GetBits(*img);
-        if (*imageptr == nullptr) {
-            cout << "Null pointer in image\n";
-            exit(1);
-        }
+
+    cout << "Do you want to rescale the image? (y/n) ";
+    char rescale;
+    cin >> rescale;
+    if (rescale == 'y' || rescale == 'Y') {
+        cout << "New width: ";
+        int width;
+        cin >> width;
+        cout << "New height: ";
+        int height;
+        cin >> height;
+        FIBITMAP *temp = *img;
+        *img = FreeImage_Rescale(temp, width, height, FILTER_BOX);
+        FreeImage_Unload(temp);
+        sizeX = FreeImage_GetWidth(*img);
+        sizeY = FreeImage_GetHeight(*img);
+        bpp = FreeImage_GetBPP(*img);
+        nChannel = bpp / 8;
+        cout << "X =" << sizeX << " Y = " << sizeY << " bpp =" << bpp << " nChannel =" << nChannel << "\n";
     }
-    swapRedwithBlue(*imageptr, sizeX, sizeY);
+    if (!(*imageptr)) {
+        delete[] (*imageptr);
+    }
+    *imageptr = FreeImage_GetBits(*img);
+    if (*imageptr == nullptr) {
+        cout << "Null pointer in image\n";
+        exit(1);
+    }
+    
+    FreeImage_Unload(*img);
 }
 // remove old halftone and create new one
 void cleanOutput () {
@@ -91,18 +97,19 @@ void cleanOutput () {
     halftone = new BYTE[imageSizeX*imageSizeY*3];
 }
 
-void saveImage(char *filename, BYTE *buf, int bpp){
+void saveImage(char *filename, BYTE *buf, int nChannel, int bpp){
     RGBQUAD color;
     FIBITMAP *im = FreeImage_Allocate(imageSizeX, imageSizeY, bpp);
-    for(int i=0; i<imageSizeY;i++)
-        for(int j=0;j<imageSizeX;j++)
-        {
-            color.rgbRed = buf[(i*imageSizeX+j)*3];
-            color.rgbGreen = buf[(i*imageSizeX+j)*3+1];
-            color.rgbBlue = buf[(i*imageSizeX+j)*3+2];
-            FreeImage_SetPixelColor(im,j,i, &color);
+    for (int i = 0; i < imageSizeY; i++) {
+        for (int j = 0; j < imageSizeX; j++) {
+            color.rgbRed = buf[(i * imageSizeX + j) * nChannel];
+            color.rgbGreen = buf[(i * imageSizeX + j) * nChannel + 1];
+            color.rgbBlue = buf[(i * imageSizeX + j) * nChannel + 2];
+            FreeImage_SetPixelColor(im, j, i, &color);
         }
+    }
     FreeImage_Save(FIF_BMP, im, filename, 0);
+    FreeImage_Unload(im);
 }
 
 //  ------- Main: Initialize glut window and register call backs ---------- 
@@ -113,9 +120,9 @@ int main(int argc, char **argv) {
     cout << "Object image file name: ";
     cin >> filename;
     loadImage(filename, imageSizeX, imageSizeY, image_bpp, image_nChannel, &image, &img);
-//    strcpy(filename, "house-bw-imp-map.png");
-//    loadImage(filename, impSizeX, impSizeY, imp_bpp, imp_nChannel, &imp_map, &imp);
     cleanOutput();
+    strcpy(filename, "rescaled.png");
+    saveImage(filename, image, image_nChannel, image_bpp);
     // generate QR code encoding a text
     strcpy(qrText, "house");
     qrCode = QRcode_encodeString(qrText, 6, QR_ECLEVEL_H, QR_MODE_8, 1);
@@ -125,12 +132,15 @@ int main(int argc, char **argv) {
     strcpy(qrFilename, "output.png");
     writePNG(halftonedQRCode, qrFilename);
     strcpy(filename, "halftone.png");
-    saveImage(filename, halftone, image_bpp);
+    saveImage(filename, halftone, image_nChannel, image_bpp);
     QRcode_free(qrCode);
     delete[] halftonedQRCode->data;
     delete halftonedQRCode;
     // read the text from the .png file of the halftoned QR code
-    int success = readQR(qrFilename);
+    int result = readQR(qrFilename);
+    if (result) {
+        cout << "Could not decode QR code." << endl;
+    }
 
     return 0;
 }
